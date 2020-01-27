517fe0e29d632e83ac60fb6c19434e43af67ec94
mohini123456 <60251427+mohini123456@users.noreply.github.com>
2020-01-27 17:29:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+The **TITLE of this Script is**
+# GUESSINGGAME
+This Script  contains the following **lines of code**:
+25
+**date and time** at which make was run:
+Mon, Jan 27, 2020  4:55:29 PM
+# unix- workbench-project-Repo
+# unix_project_guessingame
---
+++ guessinggame_final.sh
@@ -0,0 +1,25 @@
+function incorrect_response {
+if [[ $response -gt $files ]]
+then
+echo "$response is greater"
+echo "re-enter your response"
+read response
+elif [[ $response -lt $files ]]
+then
+echo "$response is smaller"
+echo "re-enter your response"
+read response
+fi
+}
+
+files=$(ls -1|wc -l)
+echo "please enter your response"
+read response
+while [[ $response -ne $files ]] 
+do
+incorrect_response $response
+done
+if [[ $response -eq $files ]]
+then
+echo "congratulations"
+fi
